1dbc1419b8481ff133efba9af458a1a9b3c4b9bc
hernan padula <hernanpadula@gmail.com>
2023-06-19 16:47:41 -0300

    correcion final

--- script1.js
+++ script1.js
@@ -1,31 +1,30 @@
 const juegos = [
-    {
-        nombre: "SIFU",
-        precio: 3099
-    },
-    {
-        nombre: "PROJECT ZOMBOID",
-        precio: 280
-    },
-    {
-        nombre: "RESIDENT EVIL 4",
-        precio: 12999
-    },
-    {
-        nombre: "OUTLAST TRIALS",
-        precio: 2599
-    },
-    {
-        nombre: "BULLY",
-        precio: 300
-    },
-    {
-        nombre: "GROUNDED",
-        precio: 1599
-    },
-    {
-        nombre: "COUNTER-STRIKE GLOBAL OFFENSIVE",
-        precio: 3219
-    }
-
-]
+  {
+    nombre: "SIFU",
+    precio: 3099,
+  },
+  {
+    nombre: "PROJECT ZOMBOID",
+    precio: 280,
+  },
+  {
+    nombre: "RESIDENT EVIL 4",
+    precio: 12999,
+  },
+  {
+    nombre: "OUTLAST TRIALS",
+    precio: 2599,
+  },
+  {
+    nombre: "BULLY",
+    precio: 300,
+  },
+  {
+    nombre: "GROUNDED",
+    precio: 1599,
+  },
+  {
+    nombre: "COUNTER-STRIKE GLOBAL OFFENSIVE",
+    precio: 3219,
+  },
+];
--- script2.js
+++ script2.js
@@ -1,15 +1,26 @@
-
-function filtrarPrecio(precio){
-    const juegosFiltrados = juegos.filter((juego) => juego.precio <= precioLimite); 
-    return juegosFiltrados;
-}
 let precioLimite = parseFloat(prompt("mostrar Juegos con precio menor a..."));
-while(precioLimite != 0){
-    if(isNaN(precioLimite)){
-        alert('ingrese un precio valido >:(');
-    }else{
-        const listaJuegos = filtrarPrecio(precioLimite);
-        console.log(listaJuegos)
-    }
-    precioLimite = parseFloat(prompt("mostrar Juegos con precio menor a..."));
+
+function filtrarPrecio() {
+  const juegosFiltrados = juegos.filter(
+    (juego) => juego.precio <= precioLimite
+  );
+  return juegosFiltrados;
 }
+
+while (precioLimite > 0) {
+  if (isNaN(precioLimite)) {
+    alert("ingrese un precio valido >:(");
+  } else {
+    const listaJuegos = filtrarPrecio(precioLimite);
+    console.log(listaJuegos);
+  }
+  continuar = prompt("Desea continuar filtrando? S para si, N para no")
+  if(continuar == "S"){
+    precioLimite = parseFloat(prompt("mostrar Juegos con precio menor a..."));
+  } else {
+    break;
+  }
+  
+}
+
+
